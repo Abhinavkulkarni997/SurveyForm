97d6cfa89c8f23821f26a33fca96d942783570b8
Abhinavkulkarni997 <Abhinavkulkarni64@gmail.com>
2025-09-13 00:14:59 +0530

    new files and features added

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <div>
+         <div className="flex h-screen items-center justify-center bg-gradient-to-r from-blue-600 to-purple-600 text-white">
+      <h1 className="text-5xl font-bold">Vite + React + Tailwind 3.4.3 🚀</h1>
+    </div>
+
+        </div>
+       
+    </>
+  )
+}
+
+export default App
