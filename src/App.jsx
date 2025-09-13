d0be475ef84e6c9300c8852be271ab1e8a52932f
Abhinav Kulkarni <Abhinavkulkarni64@gmail.com>
2025-09-14 00:47:51 +0530

    new featues added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 import './App.css'
+import SurveyForm from './components/SurveyForm'
 
 function App() {
  
 
   return (
     <>
-      <div>
-         <div className="flex h-screen items-center justify-center bg-gradient-to-r from-blue-600 to-purple-600 text-white">
-      <h1 className="text-5xl font-bold">Vite + React + Tailwind 3.4.3 🚀</h1>
-    </div>
-
-        </div>
+      <SurveyForm/>
        
     </>
   )
