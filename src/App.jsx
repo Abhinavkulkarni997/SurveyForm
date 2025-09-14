2284c82581ec59c6b01b359e5be5d021caee8385
Abhinav Kulkarni <Abhinavkulkarni64@gmail.com>
2025-09-14 23:30:23 +0530

    new featues added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import SurveyForm from './components/SurveyForm'
+import SurveyFormBackup from './components/SurveyFormBackup'
 
 function App() {
  
@@ -7,6 +8,7 @@ function App() {
   return (
     <>
       <SurveyForm/>
+      <SurveyFormBackup/>
        
     </>
   )
