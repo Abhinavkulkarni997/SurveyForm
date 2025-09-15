51f9d7f90b03ba752cf8012a95342eceee72e037
unknown <Abhinavkulkarni997>
2025-09-15 12:18:59 +0530

    new files and features added

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <SurveyForm/>
-      <SurveyFormBackup/>
+      {/* <SurveyFormBackup/> */}
        
     </>
   )
--- src/components/SurveyForm.jsx
+++ src/components/SurveyForm.jsx
@@ -1,14 +1,80 @@
 import React from 'react'
-
+import Send from '../assets/images/SubmitButton/send-symbol-svgrepo-com.svg';
 const SurveyForm = () => {
+const SurveyData=[
+  {
+    id:1,
+    question:"Hi there! I am your AI assistant. I am here to help you in filling the survey form.",
+  },
+  {
+    id:2,
+    question:"What is your name?",
+    type:"text",
+    placeholder:"Enter your name",
+    required:true
+  },
+  {
+    id:3,
+    question:"What is Your College Name?",
+    type:"text",
+    placeholder:"Enter your college name",
+    required:true
+  },
+    {
+    id:4,
+    question:"What is your email?",
+    type:"email",
+    placeholder:"Enter your email",
+    required:true
+  },
+  {
+    id:5,
+    question:"What is your phone number?",
+    type:"tel",
+    placeholder:"Enter your phone number",
+    required:true
+  },{
+    id:6,
+    question:"Please Select Your Course",
+    type:"select",
+    options:["B.Tech","M.Tech","MBA","BBA","BCA","MCA"],
+    required:true
+  },{
+    id:7,
+    question:'Please Select Your Branch Name/ Department Name',
+    type:"select",
+    options:["CSE","ECE","EEE","IT","CS-AI/ML","CS-DataScience","CSE-IoT","CSBS","CSIT","CSM","CSH","CS-Graphics","CS-DevOps","CS-CyberSecurity","CS-CloudComputing","CS-FullStack","CS-BlockChain",
+     "ME","Civil Engineering","Bio-Technology","Chemical Engineering","BPharmacy","Biomedical Engineering","Material Engineering","Aerospace Engineering"],
+    required:true
+  },{
+    id:8,
+    question:'What is your Area of Interest?',
+    type:"select",
+    options:["Natural Language Processing","Generative AI","Data Science","Computational Learning Theory","Machine Learning","Artificial Intelligence","Cloud Computing","AI/ML-Cyber Security","Internet of Things (IoT)"],
+    required:true
+  },{
+    id:9,
+    question:"Can You Provide a Brief Description About Artificial Intelligence and Machine Learning in your own words?",
+    type:"textarea",
+    placeholder:"Enter your description",
+    required:true
+  }
+]
+
   return (
-    <section className='min-h-screen py-12 px-4'>
+    <section className='min-h-screen  py-12 px-4 sm:px-6 lg:px-8 '>
      <div className='max-w-7xl mx-auto'>
-      <h1 className='text-3xl font-bold text-center my-5'>Survey Form</h1>
+     
       <div className='w-1/2 mx-auto border p-5 rounded-lg shadow-lg'>
+       <h1 className='text-3xl font-bold text-center my-5'>Student Form</h1>
+       <p className="text-lg text-indigo-600">We value your opinion</p>
       {/* <img src={}/> */}
       <h1 className='p-4 bg-indigo-500 w-full text-white'>Welcome I am Your Ai Agent </h1>
-      <button className='bg-indigo-500 text-white px-4 py-2 rounded-lg mt-4 hover:bg-indigo-600'>Start Survey</button>
+      <div className='p-4 bg-gray-200 w-full text-black my-3 rounded-lg'>Hello! I'm here to assist you with your survey. Please fill out the form below with your details and any questions or comments you may have. Your feedback is important to us!</div>
+      <div className='flex flex-row gap-2 md:flex'>
+      <input type="text" placeholder='Type Your Message....' className='w-full border p-4 rounded-lg my-3 border border-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 '/>
+      <button className='bg-indigo-500 text-white p-4 my-3 rounded-lg mt-2 hover:bg-indigo-600 inline-flex items-center gap-1'>Submit <img src={Send} alt="Send" className='inline-flex  w-4 h-4 ' /></button>
+      </div>
       </div>
      </div>
      
