daeb6f85fb9dddaf5a01809c7bdea354d9c0c9e5
unknown <Abhinavkulkarni997>
2025-09-17 12:50:51 +0530

    new files and features added

--- Backend/routes/surveyRoutes.js
+++ Backend/routes/surveyRoutes.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const survey =require('../models/Survey');
 const router=express.Router();
 router.post('/',(req,res)=>{
-    console.log(req.body);
+    try{
+        const surveyData=async 
+ console.log(req.body);
     res.json({message:'Survey response submitted successfully'});
+
+    }catch(error){
+        console.log('error in saving to DB',error);
+        res.status(500).json({error:'Error in saving  the survey data'})
+    }
+   
 });
 
 module.exports= router;
