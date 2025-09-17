2681ec31270cc5b0ade3df37785d8de0b91ba568
unknown <Abhinavkulkarni997>
2025-09-17 12:50:51 +0530

    new files and features added

--- Backend/models/Survey.js
+++ Backend/models/Survey.js
@@ -1,11 +1,16 @@
 const mongoose=require('mongoose');
 const {schema}=mongoose;
 const surveySchema=new schema({
+    id:{type:Number,required:true,unique:true},
     Name:{type:String ,required:true},
     CollegeName:{type:String,required:true},
-    Email:{type:String,required:true},
-    MobileNumber:{type:String,required:true},
+    Email:{type:String,required:true,lowercase:true,unique:true},
+    MobileNumber:{type:String,required:true,unique:true},
     Course:{type:String,required:true},
+    BranchName:{type:String,required:true},
     AreaOfInterest:{type:String,required:true},
     Description:{type:String,required:true},
-})
+    CreatedAt:{type:Date,required:true}
+  
+},{timestamp:true});
+module.exports=mongoose.model('surveySchema',surveySchema);
