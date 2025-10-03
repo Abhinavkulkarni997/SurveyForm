9baa56afed532fe6e2272763a225c21cce568462
unknown <Abhinavkulkarni997>
2025-10-03 17:39:06 +0530

    new featue added

--- src/components/StudentsData.jsx
+++ src/components/StudentsData.jsx
@@ -1,11 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useState,useEffect } from 'react'
 import axios from 'axios'
 const StudentsData = () => {
-    const [studentsSubmittedData,setStudentsSubmittedData]=React.useState([])
-        
+    const [studentsSubmittedData,setStudentsSubmittedData]=useState([]); 
     useEffect(()=>{
-
-    
             axios.get('http://localhost:5000/api/surveys')
             .then(response=>{
                 console.log("Fetched Students Data:",response.data);
@@ -26,26 +23,42 @@ const StudentsData = () => {
         <h1>Students Data</h1>
         <p>Here you can find the data of all students who have submitted the survey.</p>
         </div>
-        <div className='max-w-4xl mx-auto p-4 sm:p-8 border rounded-lg shadow-lg'>
-        {studentsSubmittedData.map((student,index)=>(
-                <table className='w-full border-collapse'>
-            <thead>
+        <div className='max-w-7xl mx-auto p-4 sm:p-8 border rounded-lg shadow-lg'>
+       
+                <table className='min-h-screen rounded-lg w-full border-collapse border border-slate-400'>
+            <thead className='bg-indigo-800 text-white  font-bold line-height:1.5 flex items-center justify-between p-2 border-b'>
+            <tr >
+                <th className='px-4 py-2 '>Sr.No</th>
+                <th className='px-4 py-2'>Student Name</th>
+                <th className='px-4 py-2'>Student College Name</th>
+                <th className='px-4 py-2'>Student Email</th>
+                <th className='px-4 py-2'>Student Mobile Number</th>
+                <th className='px-4 py-2'>Student Course</th>
+                <th className='px-4 py-2'>Student Department Name</th>
+                <th className='px-4 py-2'>Student Area Of Interest</th>
+                <th className='px-4 py-2'>Student Description</th>
+                <th className='px-4 py-2'>Student Analyzed Data</th>
+            </tr>
               
             </thead>
             <tbody>
+             {studentsSubmittedData.map((student,index)=>(
                 <tr key={index} className='border-b'>
-                    <td className='p-2 font-semibold'>Student Name:</td>
-                    <td className='p-2'>{student.name}</td>
+                    <td className='px-2 py-2 '>{student.Name}</td>
+                    <td className='px-2 py-2 '>{student.CollegeName}</td>
+                    <td className='px-2 py-2 '>{student.Email}</td>
+                    <td className='px-2 py-2 '>{student.MobileNumber}</td>
+                    <td className='px-2 py-2 '>{student.Course}</td>
+                    <td className='px-2 py-2 '>{student.DepartmentName}</td>
+                    <td className='px-2 py-2 '>{student.AreaOfInterest.join(', ')}</td>
+                    <td className='px-2 py-2 '>{student.Description}</td>
+                    <td className='px-2 py-2 '>{student.AnalyzedData.join(', ')}</td>
                     </tr>
-                    <tr className='border-b'>
-                    <td className='p-2 font-semibold'>Email:</td>  
-                    <td className='p-2'>{student.email}</td>
-                    </tr>
-         
-
+                ))}
             </tbody>
+            
         </table>
-        ))}
+       
     
         </div>
 
