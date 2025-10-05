3147d835b65e4fe0c91aefb67b598714220fe43d
Abhinav Kulkarni <Abhinavkulkarni64@gmail.com>
2025-10-05 23:24:28 +0530

    new features added

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import './App.css'
 import StudentsData from './components/StudentsData'
 import SurveyForm from './components/SurveyForm'
 import SurveyFormBackup from './components/SurveyFormBackup'
-
+import { Route,Routes } from 'react-router-dom';
 function App() {
  
 
   return (
     <>
-      <SurveyForm/>
+     
       {/* <SurveyFormBackup/> */}
-      <StudentsData/>
+      <Routes>
+        <Route path='/' element={ <SurveyForm/>}/>
+      <Route path='/admin/studentsData' element={<StudentsData/>}/>
+      </Routes>
+    
        
     </>
   )
