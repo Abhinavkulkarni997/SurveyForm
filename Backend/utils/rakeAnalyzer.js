4c6faf729a4e2514d69a9a2cfa64f421817059d3
unknown <Abhinavkulkarni997>
2025-10-06 18:03:35 +0530

    new featue added

--- Backend/utils/rakeAnalyzer.js
+++ Backend/utils/rakeAnalyzer.js
@@ -8,7 +8,17 @@ function rakeAnalyzer(text=''){
     const words=text.toLowerCase().split(/[\s,.;!?]+/);
     const filteredWords=sw.removeStopwords(words);
 
-    const keywords=rake.generate(filteredWords.join(' '));
+    const freq={};
+    filteredWords.forEach(word=>{
+        if(word.length>2){
+            freq[word]=(freq[word] || 0)+1
+        }
+})
+
+const keywords=Object.entries(freq)
+.sort((a,b)=>(b[1]-a[1]))
+.map(e=>e[0])
+const keywords=rake.generate(text);
     return {keywords};
 }
 
